7489161709b09f1929b84983f0835e566ff376c8
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-02 21:41:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 alert("Welcome my second page");
    
+let ag = [
+    {
+        id:33,
+        mane:"badhon",
+        age:2.62
+    },
+    {
+        id:33,
+        mane:"bapon",
+        age:3.00
+    },
+    {
+        id:33,
+        mane:"ma",
+        age:4.62
+    }
+];
+
+function mass(){
+  return ag.filter(function(x){
+      return x.age > 3
+   })
+   
+}
+
+console.log(mass());
