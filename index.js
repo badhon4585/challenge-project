a55c3dda9c3349d5661725cf815f6ea90f4dc9bf
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-10 16:40:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 
-   
-document.querySelectorAll(".print")[0].addEventListener("click",function(){
-    var text = this.innerHTML;
-    console.log(text)
-
-     pr(text);
-   
- });
-
-
-
- function pr(text){
-     switch(text){
-       case "print my website":
-           print()
-    }
- }
-
-
+ 
 
 
     
