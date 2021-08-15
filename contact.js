ad1c576ac25405dff9458977e797293fa8ad87ae
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-15 16:44:59 +0600

    Update contact.js

--- contact.js
+++ contact.js
@@ -4,13 +4,13 @@ const year = document.querySelector("#year");
 const btnt = document.querySelector(".btnt");
 const booklist = document.querySelector("#book-list");
 
-btnt.addEventListener('click', function(e){
+btnt.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if(title.value == '' && author.value =='' && year.value == '' ){
+    if (title.value == '' && author.value == '' && year.value == '') {
         alert('Please input your information.');
 
-    }else{
+    } else {
         const newRow = document.createElement('tr');
 
 
@@ -19,18 +19,46 @@ btnt.addEventListener('click', function(e){
         newRow.appendChild(newtitle);
 
 
-        
+
         const newauthor = document.createElement('th');
         newauthor.innerHTML = author.value;
-        newRow.appendChild( newauthor);
+        newRow.appendChild(newauthor);
 
 
 
-        
+
         const newyear = document.createElement('th');
         newyear.innerHTML = year.value;
         newRow.appendChild(newyear);
 
         booklist.appendChild(newRow);
     }
-})
+});
+
+
+
+const likeBtn = document.querySelector(".like-btn");
+let likeIcon = document.querySelector("#icon");
+let count = document.querySelector("#cont");
+
+
+let clicked = false;
+
+likeBtn.addEventListener("click", () => {
+    if (!clicked) {
+        clicked = true;
+        likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+        count.textContent++;
+
+    } else {
+        clicked = false;
+        likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+        count.textContent--;
+    }
+
+});
+
+
+
+
+
