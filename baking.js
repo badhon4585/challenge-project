8d305949680f376262dbfd6b09a0b2a7fc37904a
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-19 22:41:58 +0600

    Create baking.js

---
+++ baking.js
@@ -0,0 +1,21 @@
+var http=require('http');
+
+var server = http.createServer(function (req,res){
+
+    if(req.url=="/"){
+
+        res.writeHead(200,{'content-Type':'text/html'});
+        res.write('<h1>badhon</h1>');
+    }
+    else if(req.url=="/about"){
+
+        res.writeHead(200,{'content-Type':'text/html'});
+        res.write('<h1>bad</h1>');
+    }
+
+    else if(req.url=="/about"){
+
+        res.writeHead(200,{'content-Type':'text/html'});
+        res.write('<h1>chakarboti</h1>');
+    };
+});
