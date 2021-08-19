45d85202518c9a3021cf8b30528fa02fdcf07044
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-19 22:47:00 +0600

    Update baking.js

--- baking.js
+++ baking.js
@@ -13,10 +13,12 @@ var server = http.createServer(function (req,res){
         res.write('<h1>bad</h1>');
     }
 
-    else if(req.url=="/about"){
+    else if(req.url=="/project"){
 
         res.writeHead(200,{'content-Type':'text/html'});
         res.write('<h1>chakarboti</h1>');
     };
 });
 
+server.listen(5050);
+console.log("server run success");
