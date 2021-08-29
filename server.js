c025f49a5bd639a13243fe3de58ab03cd2e635bb
badhon4585 <badhonchakraboti47@gmail.com>
2021-08-29 23:45:28 +0600

    Update server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var fs=require('fs');
+
+var http=require('http');
+
+var swever=http.createServer(function(req,res){
+
+    if(req.url="/"){
+        fs.readFile('index.html',function(error,data){
+
+             res.write(200,{'content-type':'text/html'});
+             res.write(data);
+             res.end();
+        });
+    };
+});
+
+Server.listen(4040);
+
+console.log("badhon");
